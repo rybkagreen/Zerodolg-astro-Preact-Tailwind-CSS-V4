d8e1815bd95f619c12c048c4ce1437c3fe8f669a
rybkagreen <alex_belin@mail.ru>
2025-09-17 15:53:08 +0300

    Remove orchestration system files and related documentation

--- orchestration_system/cpp_dev_system/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core components of the C++ Development System"""
-
-from .task_manager.task_orchestrator import TaskOrchestrator
-from .vector_db.setup_qdrant import QdrantManager, OllamaEmbedder
-
-__all__ = ['TaskOrchestrator', 'QdrantManager', 'OllamaEmbedder']
